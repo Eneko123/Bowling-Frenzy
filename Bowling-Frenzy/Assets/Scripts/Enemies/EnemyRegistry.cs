6df48215aec648c790bbec4b6e7dd16d03188757
MEmilia01 <mariana_cardozo@euneiz.com>
2026-02-06 17:36:33 +0100

    Update: testeo de limite de enemigos instanciados
    el limite funciona correctamente, falta probando cuando se vaya eliminando los prefabs

--- Bowling-Frenzy/Assets/Scripts/Enemies/EnemyRegistry.cs
+++ Bowling-Frenzy/Assets/Scripts/Enemies/EnemyRegistry.cs
@@ -2,18 +2,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyRegistry : MonoBehaviour
-{
-    //variables
-    public static EnemyRegistry Instance { get; private set; }
-    private HashSet<EnemyBase> activeEnemies = new();
+{ 
     //metodos
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
+    void Start() { }
+    void Update() { }
     //
+    public static EnemyRegistry Instance { get; private set; }
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private HashSet<EnemyBase> activeEnemies = new();
+    public int ActiveCount => activeEnemies.Count;
+    public void Register(EnemyBase enemy) => activeEnemies.Add(enemy);
+    public void Unregister(EnemyBase enemy) => activeEnemies.Remove(enemy);
 }
--- Bowling-Frenzy/Assets/Scripts/Enemies/EnemySpawner.cs
+++ Bowling-Frenzy/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -4,18 +4,49 @@ public class EnemySpawner : MonoBehaviour
 {
     //variables
     public int maxActiveEnemies = 20;
+    public GameObject enemyPrefab;
+    private float spawnCooldown = 2f;
+    private float timer;
     private int currentEnemies => EnemyRegistry.Instance.ActiveCount;
 
 
     //metodos
-    void Start()
-    {
-        
-    }
+    void Start() { }
     void Update()
     {
-       if (currentEnemies < maxActiveEnemies && ShouldSpawn())
+        if (EnemyRegistry.Instance == null) return;
+
+        if (currentEnemies < maxActiveEnemies && ShouldSpawn())
         { SpawnEnemy(); }
+    }
+
+    //
+   
+    private void SpawnEnemy()
+    {
+        // Instanciar el prefab
+        GameObject go = Instantiate(enemyPrefab, transform.position, Quaternion.identity); 
+
+        // Obtener el componente EnemyBase del prefab instanciado
+        EnemyBase enemy = go.GetComponent<EnemyBase>();
+        if (enemy != null)
+        {
+            EnemyRegistry.Instance.Register(enemy);
+        }
+        else
+        {
+            Debug.LogError("El prefab no tiene EnemyBase");
+        }
+    }
+    private bool ShouldSpawn()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnCooldown)
+        {
+            timer = 0f;
+            return true;
+        }
+        return false;
     }
 
 }
