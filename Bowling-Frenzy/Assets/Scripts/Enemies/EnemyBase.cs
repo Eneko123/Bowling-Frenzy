df89df0037f82bbd6fd810bb852cf0bc09ede855
MEmilia01 <mariana_cardozo@euneiz.com>
2026-02-06 17:35:34 +0100

    Update: se le añade al prefab el script base

---
+++ Bowling-Frenzy/Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    //variable
+    public int hp = 10;
+    //numero random
+
+    //metodos
+    void Start() { }
+    void Update() { }
+    
+    //
+
+
+}
