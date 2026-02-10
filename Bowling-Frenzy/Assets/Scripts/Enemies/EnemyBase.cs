9f39b99b48c6b27dd32593ecec151d528f5abe21
Eneko123 <eneko_iglesias@euneiz.com>
2026-02-10 18:21:40 +0100

    NavMesh
    He añadido el sistema de navegación para un enemigo, pero falta pulirlo

--- Bowling-Frenzy/Assets/Scripts/Enemies/EnemyBase.cs
+++ Bowling-Frenzy/Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBase : MonoBehaviour
 {
     //variable
     public int hp = 10;
     public Transform player;
-    public CharacterController controller;
-    public float speed = 3f;
-    public float trunSpeed = 5f;
+    private NavMeshAgent agent;
     //numero random
 
     //metodos
-    void Start() { }
+    void Start() 
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
     void Update() 
     { 
         Movemetn();
@@ -19,11 +21,7 @@ public class EnemyBase : MonoBehaviour
 
     void Movemetn()
     { 
-        Vector3 direction = (player.position - transform.position).normalized;
-        direction.y = 0;
-
-        transform.forward = Vector3.RotateTowards(transform.forward, direction, trunSpeed + Time.deltaTime, 0f);
-        controller.Move(transform.forward * speed * Time.deltaTime);
+        agent.SetDestination(player.position);
     }
 
 
