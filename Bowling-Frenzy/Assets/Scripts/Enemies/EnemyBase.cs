7503398f3f9805cd27fc00b7f26d6d4b8f4230f2
Eneko123 <eneko_iglesias@euneiz.com>
2026-02-10 12:23:28 +0100

    Exportar + Movimiento del enemigo
    He exportado el modelo del enemigo de blender y he hecho le movimiento base para los enemigos

--- Bowling-Frenzy/Assets/Scripts/Enemies/EnemyBase.cs
+++ Bowling-Frenzy/Assets/Scripts/Enemies/EnemyBase.cs
@@ -4,13 +4,27 @@ public class EnemyBase : MonoBehaviour
 {
     //variable
     public int hp = 10;
+    public Transform player;
+    public CharacterController controller;
+    public float speed = 3f;
+    public float trunSpeed = 5f;
     //numero random
 
     //metodos
     void Start() { }
-    void Update() { }
-    
-    //
+    void Update() 
+    { 
+        Movemetn();
+    }
+
+    void Movemetn()
+    { 
+        Vector3 direction = (player.position - transform.position).normalized;
+        direction.y = 0;
+
+        transform.forward = Vector3.RotateTowards(transform.forward, direction, trunSpeed + Time.deltaTime, 0f);
+        controller.Move(transform.forward * speed * Time.deltaTime);
+    }
 
 
 }
