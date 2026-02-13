b1328c46a2f87e3bb47fcb63787ecbaa76450a22
Saúl Barcia Álamo <saul_barcia@euneiz.com>
2026-02-13 17:56:09 +0100

    Comportamiento Bala normal

---
+++ Bowling-Frenzy/Assets/Scripts/Player/NormalBulletBehaviour.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class NormalBulletBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float lifeTime = 3f;
+    private float currentLifeTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        direction = playerCamera.transform.forward;
+    }
+    public void Init(Vector3 initialPos, Vector3 initialDir)
+    {
+        transform.position = initialPos;
+        direction = initialDir;
+        currentLifeTime = lifeTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        if (currentLifeTime > 0)
+        {
+            currentLifeTime -= Time.deltaTime;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
