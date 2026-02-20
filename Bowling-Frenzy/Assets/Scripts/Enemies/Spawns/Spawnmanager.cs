711713d27c6b5da19ff10e4fb4ff327151f6fd44
MEmilia01 <mariana_cardozo@euneiz.com>
2026-02-20 18:46:17 +0100

    add, prefabs de los spawner y los register

---
+++ Bowling-Frenzy/Assets/Scripts/Enemies/Spawns/Spawnmanager.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnmanager : MonoBehaviour
+{
+    //variables
+    public List<EnemySpawner> enemySpawners = new List<EnemySpawner>();
+
+    //metodos
+    void Start() { }
+    void Update() { }
+    //
+}
