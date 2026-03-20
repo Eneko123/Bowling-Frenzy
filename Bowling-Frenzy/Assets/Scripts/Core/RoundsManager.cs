6ddf848951a955d7287a5151cf6cf08284381de8
Eneko123 <eneko_iglesias@euneiz.com>
2026-03-20 18:28:34 +0100

    Nav mesh reparao y round manager terminado al completo
    definitivamente, spawnea al boss

--- Bowling-Frenzy/Assets/Scripts/Core/RoundsManager.cs
+++ Bowling-Frenzy/Assets/Scripts/Core/RoundsManager.cs
@@ -98,8 +98,6 @@ public class RoundsManager : MonoBehaviour
                 timeBetwineRounds = true;
                 if (currentRound == states.Length - 1)
                 {
-                    Debug.Log(currentRound);
-                    Debug.Log(states.Length - 1);
                     isFinalRound = true;
                     currentState = states[currentRound];
                     currentTime = 3600;
