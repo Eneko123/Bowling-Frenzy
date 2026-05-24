a96195088c82d86a80900c3e595f6bfb637dfdc5
Eneko123 <eneko_iglesias@euneiz.com>
2026-05-24 20:47:02 +0200

    arreglar acumulación en el eje y

--- Bowling-Frenzy/Assets/Scripts/Player(Isaac)/MainCharacter.cs
+++ Bowling-Frenzy/Assets/Scripts/Player(Isaac)/MainCharacter.cs
@@ -340,39 +340,14 @@ public class MainCharacter : MonoBehaviour
             Vector3 move = (this.transform.forward * MoveDir.y + cameraPlayer.transform.right * MoveDir.x);
             controller.Move(move.normalized * speed * Time.deltaTime);
         }
-        //Calcula para que el jugador baje segun la gravedad
-        //if (!IsTouchingFloor())
-        //{
-            velocity.y += gravity * Time.deltaTime;
-        //}
-        controller.Move(velocity * Time.deltaTime);
-    }
-
-    bool IsTouchingFloor()
-    {
         if (controller.isGrounded && velocity.y < 0)
         {
-            velocity.y = 0;
-            return true;
+            velocity.y = -2f; // Pequenio valor negativo, no 0, para mantener isGrounded estable
         }
 
-        RaycastHit hit;
-
-        // Lanzar el raycast hacia abajo
-        if (Physics.Raycast(transform.position, -transform.up, out hit, rayDistance, floor))
-        {
-            // Debug para visualizar el ray
-            Debug.DrawRay(transform.position, -transform.up * rayDistance, Color.green);
-            if (velocity.y < 0 && controller.isGrounded) // sigue siendo una guarrada, pero ahora funciona bien
-            {
-                velocity.y = 0;
-                return false;
-            }
-            return true;
-        }
-
-        Debug.DrawRay(transform.position, -transform.up * rayDistance, Color.red);
-        return false;
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
     }
 
     void Dead()
