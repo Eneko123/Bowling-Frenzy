5eda00889d541f88c8d45f4e7fbc7c8ff93fcd07
Eneko123 <eneko_iglesias@euneiz.com>
2026-05-27 23:54:43 +0200

    Corregir faltas de ortografía

--- Bowling-Frenzy/Assets/Scripts/Player(Isaac)/Disparo/GenerateBullet.cs
+++ Bowling-Frenzy/Assets/Scripts/Player(Isaac)/Disparo/GenerateBullet.cs
@@ -284,7 +284,7 @@ public class GenerateBullet : MonoBehaviour
         if (chosen == "Normal")
             return new UpgradeOption
             {
-                label = $" Danio normal +{powerUps.normalDmgM[tier]}",
+                label = $" Daño normal +{powerUps.normalDmgM[tier]}",
                 upgrateimage = ImagenIVForUpgrate,
                 apply = () =>
                 {
@@ -303,7 +303,7 @@ public class GenerateBullet : MonoBehaviour
         {
             SpecialBullets.Explosive => new UpgradeOption
             {
-                label = $" Explosivo: danio +{powerUps.specialDmgM[tier]} / area +{Mathf.RoundToInt((powerUps.explScaleM[tier] - 1) * 100)}%",
+                label = $" Explosivo: daño +{powerUps.specialDmgM[tier]} / area +{Mathf.RoundToInt((powerUps.explScaleM[tier] - 1) * 100)}%",
                 upgrateimage = ImagenVForUpgrate,
                 apply = () =>
                 {
@@ -325,7 +325,7 @@ public class GenerateBullet : MonoBehaviour
             },
             SpecialBullets.Piercing => new UpgradeOption
             {
-                label = $" Perforante: danio +{powerUps.specialDmgM[tier]} / cantidad de perforacion +{powerUps.pierceVals[tier]}",
+                label = $" Perforante: daño +{powerUps.specialDmgM[tier]} / cantidad de perforacion +{powerUps.pierceVals[tier]}",
                 upgrateimage = ImagenVIForUpgrate,
                 apply = () =>
                 {
@@ -348,7 +348,7 @@ public class GenerateBullet : MonoBehaviour
             },
             SpecialBullets.Slowing => new UpgradeOption
             {
-                label = $" Ralentizadora: danio +{powerUps.specialDmgM[tier]} / duracion +{powerUps.slowTimeVals[tier]}s",
+                label = $" Ralentizadora: daño +{powerUps.specialDmgM[tier]} / duracion +{powerUps.slowTimeVals[tier]}s",
                 upgrateimage = ImagenVIIForUpgrate,
                 apply = () =>
                 {
